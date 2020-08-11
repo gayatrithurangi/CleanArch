811aed310b8f597ee3f1f8ef53c4d08520378640
gayatrithurangi <50740498+gayatrithurangi@users.noreply.github.com>
2020-08-11 18:35:20 +0530

    Commit Implementation of Service and Repository  GetCourse Methods

---
+++ CleanArch/CleanArch.Application/Services/CourseServices.cs
@@ -0,0 +1,26 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseServices : ICourseService
+    {
+        private ICourseRepository _courseRepository;
+
+        public CourseServices(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Model;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _ctx; //private context
+
+        public CourseRepository(UniversityDBContext ctx)
+        {
+            //ctx is injected context
+            _ctx = ctx;//Injecting context to private context
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
